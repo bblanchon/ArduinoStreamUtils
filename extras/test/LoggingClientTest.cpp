585b4ec647f1149c066562a47697d3f5a612a2c4
Benoit Blanchon <git@benoitblanchon.fr>
2022-04-12 16:07:20 +0200

    Update copyright notice

--- extras/test/LoggingClientTest.cpp
+++ extras/test/LoggingClientTest.cpp
@@ -1,5 +1,5 @@
 // StreamUtils - github.com/bblanchon/ArduinoStreamUtils
-// Copyright Benoit Blanchon 2019-2021
+// Copyright Benoit Blanchon 2019-2022
 // MIT License
 
 #include "StreamUtils/Clients/LoggingClient.hpp"
