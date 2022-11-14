f27a7b8f6836f0a7932d86f5d5c25542795dfdbf
Benoit Blanchon <git@benoitblanchon.fr>
2022-11-14 09:22:19 +0100

    All cores inherit `Arduino.h` and `WString.h` from AVR

--- extras/test/cores/esp8266/WString.h
+++
@@ -1,5 +0,0 @@
-// StreamUtils - github.com/bblanchon/ArduinoStreamUtils
-// Copyright Benoit Blanchon 2019-2022
-// MIT License
-
-#include "../avr/WString.h"
--- extras/test/cores/teensy/WString.h
+++
@@ -1,7 +0,0 @@
-// StreamUtils - github.com/bblanchon/ArduinoStreamUtils
-// Copyright Benoit Blanchon 2019-2022
-// MIT License
-
-#pragma once
-
-#include "../avr/WString.h"
