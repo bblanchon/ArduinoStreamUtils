bd25164c5bdf3fdb9fcfa7a5aa110bb0bee515cd
Benoit Blanchon <git@benoitblanchon.fr>
2022-11-14 10:47:21 +0100

    Make `Client.h` easier to compare between cores

--- extras/test/cores/teensy/Client.h
+++ extras/test/cores/teensy/Client.h
@@ -9,23 +9,14 @@
 
 using IPAddress = String;
 
-struct Client : Stream {
+class Client : public Stream {
+ public:
   virtual int connect(IPAddress ip, uint16_t port) = 0;
   virtual int connect(const char *host, uint16_t port) = 0;
   virtual uint8_t connected() = 0;
   virtual void stop() = 0;
   virtual operator bool() = 0;
 
-  // Already in Print
-  virtual size_t write(uint8_t) = 0;
-  virtual size_t write(const uint8_t *buf, size_t size) = 0;
-  virtual void flush() = 0;
-
-  // Already in Stream
-  virtual int available() = 0;
   virtual int read() = 0;
-  virtual int peek() = 0;
-
-  // Curiously not in Stream
   virtual int read(uint8_t *buf, size_t size) = 0;
 };
